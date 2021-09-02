0510e01cf5bd007f9f2c1b5ad49ef4f995d95a77
Garrett Duncan <garrettdunc@gmail.com>
2021-09-02 17:44:44 -0400

    Apply clang-format

--- Cpsc4160Game/main.cpp
+++ Cpsc4160Game/main.cpp
@@ -3,46 +3,40 @@
 #include <iostream>
 #include <string>
 
-#include <SDL2/SDL.h> 
-#include <SDL2/SDL_image.h> 
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 
-//Screen dimension
+// Screen dimension
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-SDL_Window* my_window = NULL;
-SDL_Renderer* my_renderer = NULL;
+SDL_Window *my_window = NULL;
+SDL_Renderer *my_renderer = NULL;
 SDL_Event input;
 
-void my_SDL_init(){
+void my_SDL_init() {
 
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { 
-    std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl; 
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
   }
 
-  //Enable gpu_enhanced textures
+  // Enable gpu_enhanced textures
   IMG_Init(IMG_INIT_PNG);
-  
-  my_window = SDL_CreateWindow("my_game", 
-			       SDL_WINDOWPOS_CENTERED, 
-			       SDL_WINDOWPOS_CENTERED, 
-			       SCREEN_WIDTH,
-			       SCREEN_HEIGHT, 0);
-  my_renderer = SDL_CreateRenderer(my_window,-1,0);
 
+  my_window =
+      SDL_CreateWindow("my_game", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+  my_renderer = SDL_CreateRenderer(my_window, -1, 0);
 }
 
-int main(int argc, char* argv[]) 
-{ 
+int main(int argc, char *argv[]) {
   my_SDL_init();
-  
 
-  while(true){
-    
+  while (true) {
+
     SDL_RenderClear(my_renderer);
     SDL_RenderPresent(my_renderer);
-
   }
 
   SDL_DestroyRenderer(my_renderer);
@@ -50,6 +44,6 @@ int main(int argc, char* argv[])
 
   IMG_Quit();
   SDL_Quit();
-  
-  return 0; 
+
+  return 0;
 }
