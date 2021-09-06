1acc8751d6348624b866ed7a4e17c118e8706524
Garrett Duncan <garrettdunc@gmail.com>
2021-09-06 17:34:33 -0400

    Limit fps

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+const int FPS = 60;
+const int FRAME_DURATION = 1000 / FPS;
+
 const int FRAME_WIDTH = 50;
 const int FRAME_HEIGHT = 74;
 const int NUM_FRAMES = 8;
@@ -50,18 +53,41 @@ int main(int argc, char *argv[]) {
 
   SDL_FreeSurface(temp);
 
+  int translate = 0;
+  int fps_counter = 0;
+  int last_count_start_time = 0;
+  int current_fps = 0;
+  int this_duration = 0;
+  int frame_duration = 120;
 
   while (true) {
     Uint32 ticks = SDL_GetTicks();
     Uint32 sprite = (ticks / 100) % 8;
 
     SDL_Rect srcrect = {sprite * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT};
-    SDL_Rect dstrect = {10, 10, FRAME_WIDTH, FRAME_HEIGHT};
+    SDL_Rect dstrect = {translate, 200, FRAME_WIDTH, FRAME_HEIGHT};
 
+    translate += 2;
+    if (translate > SCREEN_WIDTH)
+      translate = 0;
 
     SDL_RenderClear(my_renderer);
     SDL_RenderCopy(my_renderer, my_texture, &srcrect, &dstrect);
     SDL_RenderPresent(my_renderer);
+
+    ++fps_counter;
+    if (ticks >= last_count_start_time + 1000) {
+      last_count_start_time = ticks;
+      current_fps = fps_counter;
+      fps_counter = 0;
+    }
+
+    std::cout << current_fps << std::endl;
+
+    this_duration = SDL_GetTicks() - ticks;
+    if (this_duration < FRAME_DURATION) {
+      SDL_Delay(FRAME_DURATION - this_duration);
+    }
   }
 
   SDL_DestroyRenderer(my_renderer);
